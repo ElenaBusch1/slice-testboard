8cac858c988ecf8e6209c7318e87816829eca971
ebusch <dawillia@flx-srv-atlas.nevis.columbia.edu>
2020-12-21 10:18:49 -0500

    Clean up from Friday - fix for bright blue GUI pending

--- takeTriggerData.py
+++ takeTriggerData.py
@@ -2,10 +2,10 @@ import os, time
 
 #os.system("fpepo 0x66d0 0x102")
 os.system("fpepo 0x66d0 0x100") #trigger mode
-os.system("fpepo 0x66f0 0x02") #adc20 
+#os.system("fpepo 0x66f0 0x02") #adc20 
 #os.system("fpepo 0x66d0 0x00") #single adc mode
-#os.system("fpepo 0x66e0 0x7f0000")
-cmd = "fdaq -T -t 2 -C alldata4.dat &"
+os.system("fpepo 0x66e0 0x7f0000")
+cmd = "fdaq -T -t 2 -C alldata5.dat &"
 os.system(cmd)
 time.sleep(0.1)
-os.system("fpepo 0x66d0 0x102")
+os.system("fpepo 0x66d0 0x2")
