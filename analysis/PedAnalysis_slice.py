9eaf430e7002f9cd2e3b8604e94e016c9e055ee7
Eleanor Woodward <elw2154@kolya.nevis.columbia.edu>
2021-05-26 15:54:49 -0400

    Added mean rms to summary plots to Andrew's corrected version

--- analysis/PedAnalysis_slice.py
+++ analysis/PedAnalysis_slice.py
@@ -244,7 +244,7 @@ class AnalyzePed(object):
               ax.xaxis.set_major_locator(MaxNLocator(integer=True))
               #ax.xaxis.set_tick_params(rotation=45)
               ax.set_ylim(0,y_max*(1 + .3))
-              ax.grid(zorder = 0)
+              ax.grid(b = True,zorder = 0)
               #ax.set_xlim(42900,43100)
               if coherent:
                   av_sig = coherent[2] ; av_dsig = coherent[3]; 
@@ -338,12 +338,14 @@ class AnalyzePed(object):
     def PlotSummary(self,data_lo,data_hi,chtype, plot_dir):
 
         fig, ax = plt.subplots(1)                
-        plt.xticks(rotation = 45)
+        plt.xticks(rotation = 70)
         fig2, ax2 = plt.subplots(1)                
         for col,title,data in [('b',"LG",data_lo), ('r',"HG",data_hi)] :
 
             names, mus, stds = zip(*data)
-            ax.grid(zorder = 0)
+
+            names = [name[:2] + name[7:] for name in names] # very janky method to change 'channelxxx' --> 'chxxx' in bar labels
+            ax.grid(b = True,zorder = 0)
             ax.bar(names,mus,fill = False,ec = col, label = title, zorder = 3) 
             ax.set_title(chtype + " Mean Pedestal Value")
             ax.set_ylabel("ADC Counts")
@@ -352,15 +354,16 @@ class AnalyzePed(object):
             fig.savefig(r'{plot_dir}/{chtype}_mu_summary.png'.format(plot_dir = plot_dir,chtype = chtype) )
             
             names, mus, stds = zip(*data)
-            ax2.grid(zorder = 0)
-            plt.xticks(rotation = 45)
-            ax2.bar(names,stds,fill = False,ec = col, label = title, zorder = 3) 
+            names = [name[:2] + name[7:] for name in names] # very janky method to change 'channelxxx' --> 'chxxx' in bar labels
+            ax2.grid(b = True,zorder = 0)
+            plt.xticks(rotation = 70)
+            mean = np.mean(stds)
+            ax2.bar(names,stds,fill = False,ec = col, label = "{} mean = {:.2f}".format(title,mean), zorder = 3) 
             ax2.set_title(chtype + " Pedestal RMS")
             ax2.set_ylabel("ADC Counts")
             ax2.set_ylim(0,max(stds) + max(stds)/4)
-            means = [np.mean(stds[0]),np.mean(stds[1])]
-            mylabs = ["{} mean = {:.2f}".format("LG",means[0]), "{} mean = {:.2f}".format("HG",means[1])]
-            ax2.legend(labels = mylabs)
+            mylabs = []
+            ax2.legend()
             fig2.savefig(r'{plot_dir}/{chtype}_rms_summary.png'.format(plot_dir = plot_dir,chtype = chtype) )
 
         plt.show()
@@ -461,7 +464,7 @@ class AnalyzePed(object):
           ax.set_title("Run {name} Pairwise Noise Correlation [%], {gain} gain".format(name = self.runNo,gain = str(gain)))
           ax.set_xticks(np.arange(len(channels)+1)-.5, minor=True)
           ax.set_yticks(np.arange(len(channels)+1)-.5, minor=True)
-          ax.grid(which = "minor", color="w", linestyle='-', linewidth=3)
+          ax.grid(b = True,which = "minor", color="w", linestyle='-', linewidth=3)
           #fig.tight_layout()
           #plt.show()
           figure = plt.gcf()
@@ -559,7 +562,7 @@ def main():
 
 
     PedData.PlotRaw(plot_dir)#,chans_to_plot = ["channel079"]) #plot raw baseline samples, can specify channel or gain
-    #PedData.AnalyzeBaseline(plot_dir, runName) #make fitted baseline histogram plot + summary mean/RMS plots
+    PedData.AnalyzeBaseline(plot_dir, runName) #make fitted baseline histogram plot + summary mean/RMS plots
     #PedData.AnalyzeBaseline(plot_dir, runName,chans_to_plot = ["channel050","channel051","channel078","channel079"] ) #example specifying certain channels to analyze
   
     ### the following lines can be used to set relevant channels for Coherent Noise and Pariwise Correlation plots 
@@ -567,10 +570,9 @@ def main():
     chs_r = [66,67,70,71,74,75,78,79] #MDAC channels on right side of board
     # chs_to_plot = [("channel0" + str(no)) for no in chs_l + chs_r]
 
-    chs_to_plot = [("channel0" + str(no)) for no in ALL_CHS_LEFT + ALL_CHS_RIGHT]
-    #PedData.PlotPairwiseCorr(plot_dir, 'hilo', chs_l = MDAC_CHS_LEFT, chs_r = MDAC_CHS_RIGHT) #plot pairwise noise correlation for hi and lo gain
-    PedData.AnalyzeBaseline(plot_dir, runName,chans_to_plot = chs_to_plot) #make fitted baseline histogram plot + summary mean/RMS plots
     chs_to_plot = [("channel0" + str(no)) for no in MDAC_CHS_LEFT + MDAC_CHS_RIGHT]
+    #PedData.PlotPairwiseCorr(plot_dir, 'hilo', chs_l = MDAC_CHS_LEFT, chs_r = MDAC_CHS_RIGHT) #plot pairwise noise correlation for hi and lo gain
+    #PedData.AnalyzeBaseline(plot_dir, runName,chans_to_plot = chs_to_plot) #make fitted baseline histogram plot + summary mean/RMS plots
     PedData.PlotCoherentNoise(plot_dir,chs = chs_to_plot) #make coherent noise histogram
     PedData.PlotPairwiseCorr(plot_dir, 'hi',chs_l = MDAC_CHS_LEFT,chs_r  = MDAC_CHS_RIGHT) #plot pairwise noise corelation for hi gain only
     PedData.PlotPairwiseCorr(plot_dir, 'lo',chs_l = MDAC_CHS_LEFT, chs_r = MDAC_CHS_RIGHT)
