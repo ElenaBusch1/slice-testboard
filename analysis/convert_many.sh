dc4e8789695c9cdaa1380f4acd4e07750f627612
ancsmith <ancsmith@cern.ch>
2021-06-03 14:02:18 -0400

    added new scripts from Eleanor

---
+++ analysis/convert_many.sh
@@ -0,0 +1,7 @@
+python convert.py run0553
+python convert.py run0554
+python convert.py run0555
+python convert.py run0556
+python convert.py run0557
+python convert.py run0558
+python convert.py run0559
