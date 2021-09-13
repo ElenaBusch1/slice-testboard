454f6064f7bff5cb2a6a785f03fa5c9565e6111d
Gabriel Matos <gabriel.pinheiro.matos@cern.ch>
2021-09-13 15:32:20 -0400

    Modified AWG pulser amplitude settings for pulser run

--- standardRunsModule.py
+++ standardRunsModule.py
@@ -195,7 +195,8 @@ class STANDARDRUNS(object):
 
         #loop through amps, take data
         #standardAmps = ['0.1','1.0'] #debug amp list
-        standardAmps = ['0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9','1.0','2.0','3.0','4.0','5.0','6.0'] #AWG valid voltage range
+        #standardAmps = ['0.1','0.2','0.3','0.4','0.5','0.6','0.7','0.8','0.9','1.0','2.0','3.0','4.0','5.0','6.0'] #AWG valid voltage range
+        standardAmps = ['0.25','0.5','0.75','1.0','1.5','2.0','2.5','3.0','3.5','4.0','4.5','5.0','5.5','6.0'] #AWG valid voltage range
 
         for stepNum,amp in enumerate(standardAmps):
             print(f'Starting pulse amplitude {amp} measurements')
